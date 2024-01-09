7a09f124edcc3f65a766e0d13385f8fe148d53bc
Stefan Ihle <stefanihle@gmx.net>
2024-01-09 22:37:48 +0100

    modify workflow flile

--- .github/utils/moveIssueToBacklog.ts
+++ .github/utils/moveIssueToBacklog.ts
@@ -2,30 +2,31 @@ import { getOctokit } from '@actions/github';
 
 async function run() {
 	try {
-		// Retrieve the token from environment variables
 		const token = process.env.CODETECHIFY_ACCESS_TOKEN;
 		if (!token) {
 			throw new Error('CODETECHIFY_ACCESS_TOKEN is not provided');
 		}
 
-		// Retrieve issue number from environment variables
-		const issueNumber = parseInt(process.env.ISSUE_ID, 10);
+		const issueNumberString = process.env.ISSUE_ID;
+		if (!issueNumberString) {
+			throw new Error('ISSUE_ID is not provided');
+		}
+		const issueNumber = parseInt(issueNumberString, 10);
 		if (isNaN(issueNumber)) {
 			throw new Error('ISSUE_ID is not a valid number');
 		}
 
-		// Initialize Octokit with the provided token
 		const octokit = getOctokit(token);
 
-		// Retrieve issue details
 		const { data: issue } = await octokit.rest.issues.get({
-			owner: process.env.GITHUB_REPOSITORY_OWNER,
-			repo: process.env.GITHUB_REPOSITORY,
+			owner: process.env.GITHUB_REPOSITORY_OWNER || '',
+			repo: process.env.GITHUB_REPOSITORY || '',
 			issue_number: issueNumber,
 		});
 
-		// Check if the issue is labeled with 'feature'
-		const isFeature = issue.labels.some(label => label.name === 'feature');
+		const isFeature = issue.labels.some(
+			(label: any) => typeof label === 'object' && label.name === 'feature',
+		);
 
 		if (isFeature) {
 			console.log(`Issue titled '${issue.title}' is labeled with 'feature'.`);
@@ -35,7 +36,11 @@ async function run() {
 			);
 		}
 	} catch (error) {
-		console.error(`Error: ${error.message}`);
+		if (typeof error === 'object' && error !== null && 'message' in error) {
+			console.error(`Error: ${(error as Error).message}`);
+		} else {
+			console.error('Unknown error occurred.');
+		}
 	}
 }
 
